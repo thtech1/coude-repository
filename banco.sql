1fa44493566ebfe305f87fb922f2a5db0cade780
thtech1 <roquelima182@gmail.com>
2024-07-27 19:55:44 -0300

    attBanco

--- banco.sql
+++ banco.sql
@@ -2,9 +2,10 @@ Create database dores;
 use dores;
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` Char(02) primary key,
-  `empresa` varchar(50) NOT NULL,
+  `id` Char(02) primary key auto increment,
+  `empresa` varchar(50) NOT NULL UNIQUE,
   `seguimento` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `cnpj` CHAR(14) NOT NULL,
   `descricao` varchar(50) NOT NULL
 );
